2215a6d1aad97fa97abffee7b412532c127cac18
DigitalStrings <alec.chen21@gmail.com>
2019-05-26 01:27:47 -0700

    slfjsldkjflskdjfl

---
+++ app/src/main/java/com/garagesale/garagesale/LoginActivity.java
@@ -0,0 +1,87 @@
+package com.garagesale.garagesale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.app.*;
+import com.google.firebase.auth.*;
+import com.google.android.gms.tasks.*;
+import android.support.annotation.*;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignup;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() != null){
+            // Profile activity here
+        }
+
+        progressDialog = new ProgressDialog(this);
+        buttonSignIn = (Button) findViewById(R.id.buttonSignin);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewSignup = (TextView) findViewById(R.id.textViewSignUp);
+
+        buttonSignIn.setOnClickListener(this);
+        textViewSignup.setOnClickListener(this);
+    }
+
+    private void userLogin(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            // Email is empty
+            Toast.makeText(this, "Please enter your e-mail.", Toast.LENGTH_SHORT).show();
+
+            // Stopping the function from executing further
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            // Password is empty
+            Toast.makeText(this, "Please enter your password.", Toast.LENGTH_SHORT).show();
+
+            // Stopping the function from executing further
+            return;
+        }
+
+        // If validations are ok; make a progress bar
+        progressDialog.setMessage("Registering User...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task){
+                        // Stuff here
+                    }
+                }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonSignIn){
+            userLogin();
+        }
+        if(view == textViewSignup){
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+}
